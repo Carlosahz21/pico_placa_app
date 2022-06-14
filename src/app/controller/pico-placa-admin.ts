176a4f8d1a70e54c08e9e7b6fe393391e9b05624
Carlos Meneses <cmeneses@cobiscorp.com>
2022-06-14 02:03:17 -0500

    Method to validate if a time is in a given range.

--- src/app/controller/pico-placa-admin.ts
+++ src/app/controller/pico-placa-admin.ts
@@ -6,20 +6,36 @@ export class PicoPlacaAdmin {
 
     /**
      * Method to find the last digits of the license plate that match the day of the date entered.
-     * @param day Day of the week (Type Enum Days)
+     * @param day Day of the week. Type: Days
      * @returns An array of Numbers with  
      * @example getLastDigits(Days.MONDAY) => should return [1, 2]
      */
     getLastDigits(day: Days): Array<Number> {
-        let lastDigits: Number [] = [];
-        if ([1,2,3,4,5].includes(day)) {
+        let lastDigits: Number[] = [];
+        if ([1, 2, 3, 4, 5].includes(day)) {
             lastDigits = DaysPicoPlaca[day]
         }
-        if(lastDigits.length == 0) {
-           console.log("the day of the date entered is not in the range of Monday-Friday"); 
+        if (lastDigits.length == 0) {
+            console.log("the day of the date entered is not in the range of Monday-Friday");
         }
         return lastDigits;
     }
+
+    /**
+     * Method to validate if a time is within the "pico y placa" schedule.
+     * @param time time of the day. Type: String
+     * @returns boolean 
+     * @example inRange("16:00") => should return true
+     */
+    public inRange(time: string): boolean {
+        var val = false;
+        if (time >= "7:00:00" && time <= "9:30:00" || time >= "16:00:00" && time <= "19:30:00") {
+            val = true;
+        }
+        return val;
+    }
+
+
 
 
 }
